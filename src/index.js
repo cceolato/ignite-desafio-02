af9fa8b1c5e2480400540e7edde05a38f31418d9
Carlos Ceolato <carlos@ceolato.com.br>
2021-03-22 16:46:00 -0300

    concluido desafio 02

--- src/index.js
+++ src/index.js
@@ -9,20 +9,77 @@ app.use(cors());
 
 const users = [];
 
+function isUUID ( uuid ) {
+  let s = "" + uuid;
+
+  s = s.match('^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$');
+  if (s === null) {
+    return false;
+  }
+  return true;
+}
+
+
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const user = users.find(user => user.username === username);
+  
+  if (!user) {
+    return response.status(404).json( { error: "User not found!" } );
+  }
+  
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (user.pro || user.todos.length < 10){
+    return next();
+  }
+
+  return response.status(403);
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } =  request.headers;
+  const { id } = request.params;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user) {
+    return response.status(404).json( { error: "User not found!" } );
+  }
+
+  if(!validate(id)){
+    return response.status(400).json( { error: "Not a UUID!" } );
+  }
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if(!todo){
+    return response.status(404).json( { error: "Todo not found!" } );    
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  const user = users.find(user => user.id === id);
+  
+  if (!user) {
+    return response.status(404).json( { error: "User not found!" } );
+  }
+  
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
